e84faab3dd02ef63cdbd1595d19c2b8526514dd3
yuya tanaka <yuya.tanaka0808.w@gmail.com>
2020-09-20 00:16:13 +0900

    [add]:チームリーダーのみチームの編集ができるように変更

--- app/controllers/teams_controller.rb
+++ app/controllers/teams_controller.rb
@@ -1,7 +1,7 @@
 class TeamsController < ApplicationController
   before_action :authenticate_user!
   before_action :set_team, only: %i[show edit update destroy]
-
+  before_action :owner?, only: %i[edit update]
   def index
     @teams = Team.all
   end
@@ -55,5 +55,11 @@ class TeamsController < ApplicationController
 
   def team_params
     params.fetch(:team, {}).permit %i[name icon icon_cache owner_id keep_team_id]
+  end
+
+  def owner?
+    unless current_user == @team.owner
+      redirect_to @team, notice: '権限がありません！'
+    end
   end
 end
